44c13f888f353272a286692e889521f5f8deccca
Carla albonico <caralbonico@gmail.com>
2021-10-17 21:41:43 -0300

    agregar component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { SpanComponent } from './componentes/span/span.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SpanComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/componentes/span/span.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-span',
+  templateUrl: './span.component.html',
+  styleUrls: ['./span.component.css']
+})
+export class SpanComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
